6b69c15f8b7236b5479db5e7a04c3cb53c6995e2
dharveshks <dharvesh_ks@yahoo.co.in>
2021-04-25 11:38:10 +0530

    first one

---
+++ src/com/demo/first/R2.java
@@ -0,0 +1,20 @@
+package com.demo.first;
+
+public class R2 extends Random {
+	
+	private static void m3() {
+		
+		System.out.println("c");
+	}
+
+	
+	
+	public static void main(String[] args) {
+		
+		R2 b = new R2();
+		
+		b.m3();
+		b.m1();
+		b.m2();
+	}
+}
---
+++ src/com/demo/first/Random.java
@@ -0,0 +1,25 @@
+package com.demo.first;
+
+public class Random {
+	
+	public static  void m1() {
+	
+		System.out.println("A");
+
+	}
+	
+	public  static void m2() {
+	
+		System.out.println("B");
+	}
+	
+	public static void main(String[] args) {
+		
+		Random a = new Random();
+		
+		m1();
+		m2();
+		
+	}
+
+}
